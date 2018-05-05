70f13ce169285dfa8decf0823eadb28736cbce4a
Guillermo <memopein97@gmail.com>
2018-05-05 17:59:54 -0500

    D

--- JTUSUG/src/CONTROLLERS/Trabajador.java
+++ JTUSUG/src/CONTROLLERS/Trabajador.java
@@ -1,7 +1,28 @@
 
 package CONTROLLERS;
 
+import GUI.TrabajadorGUI;
 
 public class Trabajador {
+    TrabajadorGUI interfaz;
     
+    public Trabajador(TrabajadorGUI ui){
+        interfaz = ui;
+    }
+    
+    public void agregaTrabajador(){
+        
+    }
+
+    public void eliminaTrabajador(){
+        
+    }
+    
+    public void modificaTrabajador(){
+        
+    }
+    
+    public void listaTrabajador(){
+        
+    }
 }

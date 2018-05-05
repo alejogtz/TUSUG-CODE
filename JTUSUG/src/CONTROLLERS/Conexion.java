07bc9969acd104da94e1c7c76767ae3499e3f5b0
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-05-05 18:14:52 -0500

    Conexion modificada

--- JTUSUG/src/CONTROLLERS/Conexion.java
+++ JTUSUG/src/CONTROLLERS/Conexion.java
@@ -1,35 +1,55 @@
-package Datos;
+package CONTROLLERS;
+//URL de Descargar del Driver
+//https://jdbc.postgresql.org/download.html
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-public class Conexion{
-	private static Connection conn = null;
-	//URL de Descargar del Driver
-	//https://jdbc.postgresql.org/download.html
-	private Conexion(){}
-	
-	public void realizaConexion(){            
-            String urlDatabase =  "jdbc:postgresql://localhost:5432/Tusug"; 
-            try {
-                Class.forName("org.postgresql.Driver");
-                conn = DriverManager.getConnection(urlDatabase, "postgres", "root");
-            } catch (SQLException e) {
-                System.err.println("Ocurrio un error : "+e.getMessage());
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	}
-	
-	public static Connection getConexion(){
-		if (conn == null)
-			new Conexion().realizaConexion();
-		return conn;
-	}
-        public static void main(String [] args){
-            System.out.println(
-                    Conexion.getConexion()
-            );
+
+public class Conexion {
+
+    //Rol por defecto
+    private static String rol;
+    private static String contrasena;
+    private static Connection conn = null;
+
+    private Conexion() {
+    }
+
+    public void realizaConexion() {
+        String urlDatabase = "jdbc:postgresql://localhost:5432/Tusug";
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(urlDatabase, rol, contrasena);
+        } catch (SQLException e) {
+            System.err.println("Ocurrio un error : " + e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public static Connection getConexion() {
+        if (conn == null) {
+            new Conexion().realizaConexion();
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        Conexion.setRol("postgres");
+        Conexion.setContrasena("root");
+        System.out.println(
+                Conexion.getConexion()
+        );
+    }
+
+    public static void setRol(String rol) {
+        Conexion.rol = rol;
+    }
+
+    public static void setContrasena(String contrasena) {
+        Conexion.contrasena = contrasena;
+    }
 }

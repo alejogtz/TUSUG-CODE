c71f3b78390fcb4f4a3a27e146f450c5408d1eef
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-05-05 18:53:17 -0500

    Guardar para pull

--- JTUSUG/src/CONTROLLERS/Autobus.java
+++ JTUSUG/src/CONTROLLERS/Autobus.java
@@ -1,10 +1,24 @@
 
 package CONTROLLERS;
 
+import GUI.AutobusGUI;
+
 /**
  *
  * @author Alekhius
  */
 public class Autobus {
+    public AutobusGUI ui;
+    public Autobus(AutobusGUI ui){
+        this.ui = ui;
+    }
+    
+    public void ingresarAutobus(){
+        
+    }
+    
+    public void eliminarTrabajador(){
+        
+    }
     
 }

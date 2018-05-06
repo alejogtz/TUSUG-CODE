2546eec5731d7eda49fc269c904be1b0bfb4ac9b
Alekhius <alejo.gutierrez@itoaxaca.edu.mx>
2018-05-06 00:06:28 -0500

    Agregue una funcion a Conexion

--- JTUSUG/src/CONTROLLERS/Conexion.java
+++ JTUSUG/src/CONTROLLERS/Conexion.java
@@ -1,10 +1,4 @@
 package CONTROLLERS;
-<<<<<<< HEAD
-=======
-//URL de Descargar del Driver
-//https://jdbc.postgresql.org/download.html
-
->>>>>>> 8840b9aebe1899d36b9e81b4f8ec6aae7bbe557f
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -53,6 +47,11 @@ public class Conexion {
     }
 
     public static void setContrasena(String contrasena) {
+        Conexion.contrasena = contrasena;
+    }
+    
+    public static void setConfiguracion(String rol, String contrasena){
+        Conexion.rol = rol;
         Conexion.contrasena = contrasena;
     }
 }

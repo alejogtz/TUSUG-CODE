4e726b27f7a7212215b7ac176fc96e28cc66f43c
christianbarto <elmostachon12@gmail.com>
2018-05-31 18:17:06 -0500

    ajuerza

---
+++ JTUSUG/src/GUI/ReportesGUI.java
@@ -0,0 +1,33 @@
+package GUI;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javafx.scene.layout.Border;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+public class ReportesGUI {
+    String ruta = "src/imagenes/";
+    JFrame f;
+    JPanel p;
+    public JTable tabla;
+    public ReportesGUI()
+    {
+        f = Builder.construirFrame("Reportes", new Rectangle(0,0, 700, 600), false); 
+        p = Builder.crearPanel(f, new Rectangle(0, 0, 700, 600),ruta+"img_fondo_ventana_facturas.png", false);
+        
+    }
+    public static void main(String []agrs)
+    {
+        ReportesGUI r = new ReportesGUI();
+    }
+}
